f51273e5084e7a0cc86d4e4f1ca951b18c9fe018
stevannn <stewica@live.com>
2026-03-30 21:17:37 +0200

    fixing failing build 5

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import path from "path";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin(
-  path.resolve(process.cwd(), "i18n/request.ts"),
+  path.join(process.cwd(), "src/i18n/request"),
 );
 
 export default withNextIntl({});
