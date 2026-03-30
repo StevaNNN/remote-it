8a3a2db158a13c17d7e0e9b2e61c87c60e15ab21
stevannn <stewica@live.com>
2026-03-30 21:26:09 +0200

    fixing failing build 8

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 // next.config.ts
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+const withNextIntl = createNextIntlPlugin();
 
 export default withNextIntl({});
