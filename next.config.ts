72ddacccdf4f066323e52e033d483f350d9d0432
stevannn <stewica@live.com>
2026-03-31 10:00:45 +0200

    fixing failing build 19

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {},
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
