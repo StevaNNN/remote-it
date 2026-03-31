0fd86b8afa30d6b1c634f4b52524ffccb0f54e75
stevannn <stewica@live.com>
2026-03-31 12:36:08 +0200

    st

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-  output: "export", // ← generates ./out folder
-  images: {
-    unoptimized: true, // ← required for static export
-  },
-};
+const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 export default withNextIntl(nextConfig);
